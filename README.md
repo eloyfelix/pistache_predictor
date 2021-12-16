0ed1f77a2026c1aef5d307eb19f4f6b00f2861af
Eloy Felix <eloyfelix@gmail.com>
2021-12-16 20:27:34 +0000

    multitask model and software version update

--- README.md
+++ README.md
@@ -1,37 +1,23 @@
 # Target predictions C++ REST microservice with ONNX Runtime, RDKit and Pistache
 
-Example of an [histone deacetylase 3](https://www.ebi.ac.uk/chembl/target_report_card/CHEMBL1829/) multi-layer perceptron "dummy" model trained in Python using [PyTorch](https://pytorch.org/)(1.4), [exported](https://pytorch.org/docs/stable/onnx.html) to the [ONNX](https://onnx.ai/) format and used with the [ONNX Runtime](https://microsoft.github.io/onnxruntime/) in C++ as a [Pistache](https://github.com/oktal/pistache) powered REST microservice. Molecular fingerprints calculated with [RDKit](https://www.rdkit.org/docs/index.html).
-
-This miniproject only pretends to be an example of how the different components can be glued together in C++ using a model previously trained in Python.
+C++ REST microservice of the [ChEMBL Multitask Model](https://github.com/chembl/chembl_multitask_model/) using [ONNX Runtime](https://microsoft.github.io/onnxruntime/) and [Pistache](https://github.com/oktal/pistache). Molecular fingerprints calculated with [RDKit](https://www.rdkit.org/docs/index.html).
 
 ## To run it
 
 ### Build the Docker image and run a container
 
 ```bash
-docker build -t pistache_predictor .
-docker run -p9999:9999 pistache_predictor
+docker-compose up -d
 ```
 
 ### Query the service:
 
 ```bash
-curl --request POST -d "Cc1ccccc1-c1ccc(C=NNC(=O)CCCCCC(=O)NO)cc1" http://localhost:9999/predict
+curl --request POST -d "Cc1ccccc1-c1ccc(C=NNC(=O)CCCCCC(=O)NO)cc1" http://localhost:9080/predict
 ```
 
-The trained model is included in the repository but it can be reproduced with the following dataset and script:
+### To stop and delete the running container
 
-- [Dataset](https://github.com/eloyfelix/pistache_predictor/blob/master/training/CHEMBL1829.csv?raw=true) extracted from [ChEMBL](https://www.ebi.ac.uk/chembl).
-- Python [script](https://github.com/eloyfelix/pistache_predictor/blob/master/training/train_demo_model.py) to train the model.
-
-## Results comparison
-
-Prediction results of the PyTorch model and of the ONNX exported model running both in Python and C++ ONNX runtimes are compared with the [compare_predictions.py](https://github.com/eloyfelix/pistache_predictor/blob/master/compare_predictions.py) script. Results are the same for all of them.
-
-## LibTorch
-
-The pistache micro-service is also alternatively implemented using the LibTorch C++ backend. It can be used by replacing the Dockerfile, CMakeLists.txt and server.cc files with:
-
-- [Dockerfile_libtorch](https://github.com/eloyfelix/pistache_predictor/blob/master/Dockerfile_libtorch)
-- [CMakeLists.txt_libtorch](https://github.com/eloyfelix/pistache_predictor/blob/master/CMakeLists.txt_libtorch)
-- [server.cc_libtorch](https://github.com/eloyfelix/pistache_predictor/blob/master/src/server.cc_libtorch)
+```bash
+docker-compose down
+```
--- src/server.cc
+++ src/server.cc
@@ -8,19 +8,20 @@
 #include <GraphMol/Fingerprints/MorganFingerprints.h>
 
 #include <onnxruntime_cxx_api.h>
-
 #include <nlohmann/json.hpp>
+#include <map>
 
 using namespace Pistache;
 using json = nlohmann::json;
 
+
 namespace Generic
 {
 
-void handleReady(const Rest::Request &, Http::ResponseWriter response)
-{
-    response.send(Http::Code::Ok, "1");
-}
+    void handleReady(const Rest::Request &, Http::ResponseWriter response)
+    {
+        response.send(Http::Code::Ok, "1");
+    }
 
 } // namespace Generic
 
@@ -29,27 +30,25 @@ class TargetPredictions
 public:
     TargetPredictions(Address addr) : httpEndpoint(std::make_shared<Http::Endpoint>(addr))
     {
-        try
-        {
-            env = std::move(Ort::Env(ORT_LOGGING_LEVEL_WARNING, "test"));
-            // initialize session options if needed and load the model
-            Ort::SessionOptions session_options;
-            session_options.SetIntraOpNumThreads(1);
-            session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
-            session = std::move(Ort::Session(env, "/app/src/mlp.onnx", session_options));
+        env = std::move(Ort::Env(ORT_LOGGING_LEVEL_WARNING, "test"));
+        // initialize session options if needed and load the model
+        Ort::SessionOptions session_options;
+        session_options.SetIntraOpNumThreads(1);
+        session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+        session = std::move(Ort::Session(env, "/app/src/chembl_multitask.onnx", session_options));
 
-            // there is only one input node in this model, using index 0 to get its info
-            Ort::AllocatorWithDefaultOptions allocator;
-            input_node_names.push_back(session.GetInputName(0, allocator));
-            Ort::TypeInfo type_info = session.GetInputTypeInfo(0);
-            auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
-            ONNXTensorElementDataType type = tensor_info.GetElementType();
-            input_node_dims = tensor_info.GetShape();
-        }
-        catch (...)
-        {
-            std::cerr << "error loading the model" << std::endl;
-        }
+        // there is only one input node in this model, using index 0 to get its info
+        Ort::AllocatorWithDefaultOptions allocator;
+        input_node_names.push_back(session.GetInputName(0, allocator));
+
+        // get target names
+        size_t output_num_nodes = session.GetOutputCount();
+        for (int i = 0; i < output_num_nodes; i++)
+            output_node_names.push_back(session.GetOutputName(i, allocator));
+        Ort::TypeInfo type_info = session.GetInputTypeInfo(0);
+        auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
+        ONNXTensorElementDataType type = tensor_info.GetElementType();
+        input_node_dims = tensor_info.GetShape();
     }
 
     void init(size_t thr = 2)
@@ -75,52 +74,58 @@ private:
 
     void predict(const Rest::Request &request, Http::ResponseWriter response)
     {
+        std::string smiles(request.body());
+        std::unique_ptr<RDKit::ROMol> mol;
+
         try
         {
-            std::string smiles(request.body());
-
-            std::unique_ptr<RDKit::ROMol> mol(RDKit::SmilesToMol(smiles));
-            if (!mol)
-            {
-                std::cerr << "Cannot create molecule from : '" << smiles << "'" << std::endl;
-                response.send(Http::Code::Internal_Server_Error, "Cannot create molecule from : '" + smiles + "'");
-            }
-            else
-            {
-                // calc fingerprints
-                std::unique_ptr<RDKit::SparseIntVect<std::uint32_t>> fp(RDKit::MorganFingerprints::getHashedFingerprint(*mol, 2, input_node_dims[0]));
-                std::vector<float> input_tensor_values(input_node_dims[0], 0);
-                for (const auto &iter : fp->getNonzeroElements())
-                    input_tensor_values[iter.first] = 1;
-
-                // create input tensor object from data values
-                auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
-                Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_tensor_values.data(), input_node_dims[0], input_node_dims.data(), 1);
-
-                // // score model & input tensor, get back output tensor
-                auto output_tensors = session.Run(Ort::RunOptions{nullptr}, input_node_names.data(), &input_tensor, 1, output_node_names.data(), 1);
-                std::vector<float> predVec(output_tensors.front().GetTensorMutableData<float>(), output_tensors.front().GetTensorMutableData<float>() + output_tensors.size());
-
-                json output;
-                output["smiles"] = smiles;
-                output["pred"] = predVec;
-
-                response.send(Http::Code::Ok, output.dump());
-            }
+            mol.reset(RDKit::SmilesToMol(smiles));
         }
         catch (RDKit::MolSanitizeException &e)
         {
             std::cerr << e.what() << std::endl;
             response.send(Http::Code::Internal_Server_Error, e.what());
         }
+
+        if (!mol)
+        {
+            std::cerr << "Cannot create molecule from : '" << smiles << "'" << std::endl;
+            response.send(Http::Code::Internal_Server_Error, "Cannot create molecule from : '" + smiles + "'");
+        }
+        else
+        {
+            // calc fingerprints
+            std::unique_ptr<RDKit::SparseIntVect<std::uint32_t>> fp(RDKit::MorganFingerprints::getHashedFingerprint(*mol, 2, input_node_dims[0]));
+            std::vector<float> input_tensor_values(input_node_dims[0], 0); // default value 0
+            for (const auto &iter : fp->getNonzeroElements())
+                input_tensor_values[iter.first] = 1;
+
+            // create input tensor object from data values
+            auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+            Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_tensor_values.data(), 
+                                                                        input_node_dims[0], input_node_dims.data(), 1);
+
+            // score model & input tensor, get back output tensor
+            auto output_tensors = session.Run(Ort::RunOptions{nullptr}, 
+                                                input_node_names.data(), &input_tensor, 1, 
+                                                output_node_names.data(), output_node_names.size());
+
+            // create the json output and send response
+            std::map<std::string, float> res_map;
+            for (int i = 0; i < output_tensors.size(); i++)
+                res_map[output_node_names[i]] = *output_tensors[i].GetTensorMutableData<float>();
+
+            json out(res_map);
+            response.send(Http::Code::Ok, out.dump());
+        }
     }
 
     Ort::Env env{nullptr};
     Ort::Session session{nullptr};
 
-    std::vector<const char*> input_node_names;
-    std::vector<const char*> output_node_names = {"output"};
-    std::vector<int64_t> input_node_dims;  
+    std::vector<const char *> input_node_names;
+    std::vector<const char *> output_node_names;
+    std::vector<int64_t> input_node_dims;
 
     Rest::Router router;
     std::shared_ptr<Http::Endpoint> httpEndpoint;
